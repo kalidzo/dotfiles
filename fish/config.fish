2f81193fb53330e8316361d3aa6eae45b2828ba0
Shahruz <106928351+kalidzo@users.noreply.github.com>
2022-09-22 03:09:49 +0500

    dotfiles

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set fish_greeting
+
+
+alias cbg="feh --randomize --bg-scale ~/.wallpaper/*"
+alias ll="exa -l --icons"
+alias lla="exa -l -a --icons"
+
